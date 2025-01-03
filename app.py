5bfe49ce3020d5f36ae276a563662cbfed13ec12
sathvik3103 <divilisathvik@gmail.com>
2025-01-02 17:25:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import time
 
 load_dotenv()
 
-api_key = os.getenv("GROQ_API_KEY")
+#api_key = os.getenv("GROQ_API_KEY")
+api_key = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=api_key)
 
 LANGCHAIN_API_KEY= os.getenv("LANGCHAIN_API_KEY")
